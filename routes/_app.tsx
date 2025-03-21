1b2ae2627d5bfe5e01d68ea4fe6f25f4a74a3e78
Sarah Sachs <zarahzachz@users.noreply.github.com>
2025-03-21 12:29:27 -0400

    🚧 Add placeholder home content/design

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>batman-horse</title>
+        <title>Home | Batman Horse</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,13 +1,29 @@
 export default function Home() {
   return (
-    <div>
-      <div>
-        <h1>Welcome to Fresh</h1>
-        <p>
-          Try updating this message in the
-          <code>./routes/index.tsx</code> file, and refresh.
-        </p>
-      </div>
-    </div>
+    <main class="trim-width">
+      <article class="welcome">
+        <h1>
+          <span>Hello,</span>
+          <span class="intro">I'm Sarah</span>
+          <span>and I'm an Interface Engineer from Ohio.</span>
+        </h1>
+
+        <p id="contacts">You can find me on the following sites:</p>
+
+        <ul role="list" aria-labelledby="contacts" class="social-links">
+          <li>
+            <a href="https://codepen.io/zarahzachz" class="social">CodePen</a>
+          </li>
+          <li>
+            <a href="https://github.com/zarahzachz" class="social">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/sarahsachs/" class="social">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </article>
+    </main>
   );
 }
